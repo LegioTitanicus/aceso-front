19533117f6dd68ed9624461b209ea7617d021457
LegioTitanicus <alexbernstein7170@gmail.com>
2020-08-20 22:46:08 -0400

    WIP, publishing for fork

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+// import React from "react";
+// import { render } from "@testing-library/react";
+// import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test("renders learn react link", () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });j
+
+//
+
+// import React from "react";
+// import Enzyme, { mount } from "enzyme";
+// import Adapter from "enzyme-adapter-react-16";
+// Enzyme.configure({ adapter: new Adapter() });
+// import App from "./App";
+j;
+describe("Testing our testing ability", () => {
+  it("App component renders elements", () => {
+    expect(page).toHave("Test render");
+    // expect(document.getElementsByTagName("h1")[0].innerText).not.toBe(
+    //   "This page isn’t working"
+    // );
+  });
 });
