2125e969fdce3eaa4f6dacf1caf0e5abe988ad15
BenjaminFairbank <BenFairbank26@gmail.com>
2020-08-24 04:53:32 -0400

    'Page' scrolls beneath appbar w/elevation effect

--- src/ui/AppBar.js
+++ src/ui/AppBar.js
@@ -6,9 +6,9 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import { useMediaQuery, Grid } from "@material-ui/core";
-import theme from "./Theme";
-
+import theme from "./Theme"
 import NavTabs from "./NavTabs";
 
 const useStyles = makeStyles((theme) => ({
@@ -26,36 +26,50 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+function ElevationScroll(props) {
+  const { children } = props;
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
 const AcesoAppBar = () => {
   const classes = useStyles();
   const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
-
   const companyName = !isDesktop ? "Aceso Clinical Solutions" : "ACS";
+  
   return (
     <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          {!isDesktop && (
-            <IconButton
-              edge="start"
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="menu"
-            >
-              <MenuIcon />
-            </IconButton>
-          )}
-          <Typography variant="h6" className={classes.title}>
-            {companyName}
-          </Typography>
-          {isDesktop && (
-            <Grid container justify="center">
-              <NavTabs />
-            </Grid>
-          )}
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
+      <ElevationScroll>
+        <AppBar position="fixed">
+          <Toolbar>
+            {!isDesktop && (
+              <IconButton
+                edge="start"
+                className={classes.menuButton}
+                color="inherit"
+                aria-label="menu"
+              >
+                <MenuIcon />
+              </IconButton>
+            )}
+            <Typography variant="h6" className={classes.title}>
+              {companyName}
+            </Typography>
+            {isDesktop && (
+              <Grid container justify="center">
+                <NavTabs />
+              </Grid>
+            )}
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
     </div>
   );
 };
